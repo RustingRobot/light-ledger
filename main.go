0ed6fc4f40d0ad658657400ff1c51d4a832174c2
RustingRobot <RustingRobot@protonmail.com>
2024-12-30 03:26:33 +0100

    playing around with texture filters

--- main.go
+++ main.go
@@ -13,15 +13,47 @@ func main() {
 
 	// OpenSans licensed under the SIL Open Font License Version 1.1
 	// https://openfontlicense.org/open-font-license-official-text/
-	var better_font = rl.LoadFontEx("OpenSans-Regular.ttf", 300, nil, 256)
+	var font = rl.LoadFontEx("OpenSans-Medium.ttf", 512, nil, 0)
+	rl.GenTextureMipmaps(&font.Texture)
+	rl.SetTextureFilter(font.Texture, rl.FilterBilinear)
+
+	var cur_filter = 0
+	var text = "point"
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("welcome world!", 190, 200, 20, rl.LightGray)
-		rl.DrawTextEx(better_font, "custom text. How cool!", rl.Vector2{X: 190, Y: 300}, 50, 1, rl.Gray)
-
-		rl.DrawRectangle(20, 20, 300, 50, rl.Red)
+		if rl.IsKeyPressed(rl.KeySpace) {
+			cur_filter++
+			if cur_filter > 5 {
+				cur_filter = 0
+			}
+			switch cur_filter {
+			case 0:
+				rl.SetTextureFilter(font.Texture, rl.FilterPoint)
+				text = "point"
+			case 1:
+				rl.SetTextureFilter(font.Texture, rl.FilterBilinear)
+				text = "bilinear"
+			case 2:
+				rl.SetTextureFilter(font.Texture, rl.FilterTrilinear)
+				text = "trilinear"
+			case 3:
+				rl.SetTextureFilter(font.Texture, rl.FilterAnisotropic4x)
+				text = "anisotropic 4x"
+			case 4:
+				rl.SetTextureFilter(font.Texture, rl.FilterAnisotropic8x)
+				text = "anisotropic 8x"
+			case 5:
+				rl.SetTextureFilter(font.Texture, rl.FilterAnisotropic16x)
+				text = "anisotropic 16x"
+			}
+		}
+		rl.DrawText(text, 20, 100, 20, rl.LightGray)
+		rl.DrawTextEx(font, "custom text. How cool!", rl.Vector2{X: 20, Y: 170}, 15, 1, rl.Gray)
+		rl.DrawTextEx(font, "custom text. How cool!", rl.Vector2{X: 20, Y: 200}, 20, 1, rl.Gray)
+		rl.DrawTextEx(font, "custom text. How cool!", rl.Vector2{X: 20, Y: 230}, 40, 1, rl.Gray)
+		rl.DrawTextEx(font, "custom text. How cool!", rl.Vector2{X: 20, Y: 260}, 60, 1, rl.Gray)
 
 		rl.EndDrawing()
 	}
