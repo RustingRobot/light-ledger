7cf31372db28d3df9f2be113531e4e994e7e441c
RustingRobot <RustingRobot@protonmail.com>
2025-02-07 21:04:57 +0100

    prevent empty tags and add border for added tags

--- ui/elements/tagManager.go
+++ ui/elements/tagManager.go
@@ -23,6 +23,9 @@ func NewTagManager(X, Y, width, height int32, color rl.Color) *TagManager {
 }
 
 func (r *TagManager) addTag() {
+	if r.input.Text == "" {
+		return
+	}
 	r.addedTags = append(r.addedTags, r.input.Text)
 	r.input.ClearText()
 	fmt.Println(r.addedTags)
@@ -33,8 +36,10 @@ func (r *TagManager) Draw(ctx *ui.UiBundle) {
 	r.add.Draw(ctx)
 	cur_x_pos := int32(0)
 	for _, e := range r.addedTags {
-		ctx.Text_renderer.DrawText(e, r.x+r.width+cur_x_pos+10, r.y+5, r.color)
-		cur_x_pos += int32(ctx.MeasureText(e).X) + 10
+		txt_width := int32(ctx.MeasureText(e).X)
+		rl.DrawRectangle(r.x+r.width+cur_x_pos+6, r.y, txt_width+8, 28, rl.Gray)
+		ctx.Text_renderer.DrawText(e, r.x+r.width+cur_x_pos+10, r.y+5, rl.DarkGray)
+		cur_x_pos += txt_width + 14
 	}
 }
 
